a1311c11beea985ed03d99eaa8623a93fe994ba8
root <root@TALIS12.localdomain>
2025-01-16 11:44:51 +0100

    djfkds,f,dsfj

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
 for i in range(10):
-    for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if i < 8 or j < 9 else "\n")
-
+    for j in range(10):
+        if (i != j and i < j) and i < 9:
+            if (i == 8 and j == 9):
+                print('{0}{1}'.format(i, j))
+            else:
+                print('{0}{1}, '.format(i, j), end='')
