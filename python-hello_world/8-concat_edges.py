faf278c2bac56540e381d7f22e091ec2a25e2d26
root <root@TALIS12.localdomain>
2025-01-16 11:20:18 +0100

    aupdate

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,6 +3,5 @@ str = (
     "Python is an interpreted, interactive, object-oriented programming "
     "language that combines remarkable power with very clear syntax"
 )
-# Extraction des parties spécifiques de la chaîne
-new_str = str[39:66] + str[106:112] + str[0:6]
-print(new_str)
+str = str[39:67] + str[107:112] + str[0:6]
+print(str)
