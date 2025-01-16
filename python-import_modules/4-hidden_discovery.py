ad2d3c86fd4625b170b6a8bcd69f4a1ae03d9850
root <root@TALIS12.localdomain>
2025-01-16 12:11:15 +0100

    fuck tchecker of 4-

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
- import hidden_4
-for i in dir(hidden_4):
-if i[:2] != "__":
- print(i)
+    import hidden_4
+    # Récupérer tous les noms définis dans le module hidden_4
+    names = dir(hidden_4)
+    # Filtrer et trier les noms
+    for name in sorted(names):
+        if not name.startswith("__"):
+            print(name)
+
