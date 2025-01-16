f89bb12ed2155b3069fbc6c8d014733895d438bb
root <root@TALIS12.localdomain>
2025-01-16 11:55:36 +0100

    jdkjfskdjf

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
-import hidden_4
-
-if __name__ == '__main__':
-    def_names = dir(hidden_4)
-
-    for i in range(len(def_names)):
-        if def_names[i][:2] != '__':
-            print(def_names[i])
+#!/usr/bin/python3.4
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
