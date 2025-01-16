02b83e971ecd5842c37204ce5033cbf80a7e255b
root <root@TALIS12.localdomain>
2025-01-16 11:22:40 +0100

    aupdate

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print('{}{}{}'.format(str, str, str))
-print('{:.9}'.format(str))
+print('{}{}{}'.format(str, str, str))  # Affiche "Holberton School" trois fois
+print('{}'.format(str[:9]))  # Affiche les 9 premiers caractères de "Holberton School"
+
